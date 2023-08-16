6ed588e34fc0dfb168411ef5230bb82daeada7b1
Saif Ali Khan <saffellikhan@gmail.com>
2023-08-16 18:49:07 +0500

    Delete users transaction updated

--- jobs/delete-users.ts
+++ jobs/delete-users.ts
@@ -5,64 +5,69 @@ import { AccountModel } from "@Models/account.ts";
 import { CollaboratorModel } from "@Models/collaborator.ts";
 import { daily } from "cron";
 
-export const PermanentlyDeleteUsers = new Transaction();
+export const PermanentlyDeleteUsers = Transaction.add(async (_, next) => {
+  const Users = await UserModel.find(
+    { deletionAt: { $lt: new Date() } },
+    { deletionAt: 0 }
+  ).catch(() => {
+    // Do nothing...
+  });
 
-PermanentlyDeleteUsers.add(async (_, next) => {
-  const Session = await mongoose.startSession();
+  const Transactions: Array<Transaction> = [];
 
-  try {
-    const Users = await UserModel.find(
-      { deletionAt: { $lt: new Date() } },
-      { deletionAt: 0 }
-    ).catch(() => {
-      // Do nothing...
-    });
+  if (Users?.length)
+    for (const User of Users)
+      Transactions.push(
+        Transaction.add(async (_, next) => {
+          const Session = await mongoose.startSession();
 
-    if (Users?.length) {
-      Session.startTransaction();
+          try {
+            Session.startTransaction();
 
-      const UserIDs = Users.map((user) => user._id);
+            await UserModel.deleteOne({ _id: User._id }).session(Session);
 
-      await UserModel.deleteMany({
-        _id: { $in: UserIDs },
-      }).session(Session);
+            const Accounts = await AccountModel.find({
+              createdFor: User._id,
+            }).session(Session);
 
-      const Accounts = await AccountModel.find({
-        createdFor: { $in: UserIDs },
-      }).session(Session);
+            const AccountIDs = Accounts.map((account) => account._id);
 
-      const AccountIDs = Accounts.map((account) => account._id);
+            await AccountModel.deleteMany({
+              _id: { $in: AccountIDs },
+            }).session(Session);
 
-      await AccountModel.deleteMany({
-        _id: { $in: AccountIDs },
-      }).session(Session);
+            const Collaborations = await CollaboratorModel.find({
+              account: { $in: AccountIDs },
+            }).session(Session);
 
-      const Collaborations = await CollaboratorModel.find({
-        account: { $in: AccountIDs },
-      }).session(Session);
+            const CollaboratorIDs = Collaborations.map(
+              (collaboration) => collaboration._id
+            );
 
-      const CollaboratorIDs = Collaborations.map(
-        (collaboration) => collaboration._id
+            await CollaboratorModel.deleteMany({
+              _id: { $in: CollaboratorIDs },
+            }).session(Session);
+
+            await next({
+              user: User,
+              collaborations: Collaborations,
+              accounts: Accounts,
+            });
+
+            await Session.commitTransaction();
+          } catch (error) {
+            await Session.abortTransaction();
+            console.error(error);
+          } finally {
+            await Session.endSession();
+          }
+        })
       );
 
-      await CollaboratorModel.deleteMany({
-        _id: { $in: CollaboratorIDs },
-      }).session(Session);
+  await next({ eachDeletion: Transactions });
 
-      await next({
-        users: Users,
-        collaborations: Collaborations,
-        accounts: Accounts,
-      });
-
-      await Session.commitTransaction();
-    }
-  } catch (error) {
-    await Session.abortTransaction();
-    console.error(error);
-  } finally {
-    await Session.endSession();
-  }
+  for (const Transaction of Transactions)
+    await Transaction.exec().catch(console.error);
 });
 
 export default async () => {
