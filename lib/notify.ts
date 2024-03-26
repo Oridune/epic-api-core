23ddf586677745d4e5290bd7cce5873e0f6b9a38
Saif Ali Khan <62799255+Saff-Elli-Khan@users.noreply.github.com>
2024-03-26 22:25:32 +0500

    Update notify.ts

--- lib/notify.ts
+++ lib/notify.ts
@@ -4,13 +4,21 @@ import { UserModel } from "@Models/user.ts";
 
 export const Novu = new _Novu(Env.getSync("NOVU_API_KEY"));
 
+export type NovuTriggerPayload = ITriggerPayloadOptions["payload"];
+
 export type NovuTriggerOptions = {
   subscriberId?: string;
   userFilter?: Parameters<(typeof UserModel)["findOne"]>[0];
   email?: string;
   phone?: string;
   template: string;
-  payload: ITriggerPayloadOptions["payload"];
+  payload: NovuTriggerPayload;
+};
+
+export type NovuManyTriggerOptions = {
+  subscriberIds: string[];
+  template: string;
+  payload: NovuTriggerPayload;
 };
 
 export class Notify {
@@ -41,6 +49,25 @@ export class Notify {
 
     await Novu.trigger(options.template, {
       to: { subscriberId: SubscriberId, ...SubscriberData },
+      payload: options.payload,
+    }).catch((error) => {
+      if (error.response.data.message === "workflow_not_found") {
+        throw new Error(`Notification workflow template not found!`, {
+          cause: error,
+        });
+      } else throw new Error(error.response.data.message, { cause: error });
+    });
+  }
+
+  static async sendManyWithNovu(
+    options: NovuManyTriggerOptions,
+  ) {
+    if (!options.subscriberIds.length) {
+      throw new Error(`No subscriber id provided!`);
+    }
+
+    await Novu.trigger(options.template, {
+      to: options.subscriberIds.map((subscriberId) => ({ subscriberId })),
       payload: options.payload,
     }).catch((error) => {
       if (error.response.data.message === "workflow_not_found") {
