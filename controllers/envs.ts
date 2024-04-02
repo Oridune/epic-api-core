b1888a804b6ead1dae0eb4d05628e229505f7e8a
Saif Ali Khan <62799255+Saff-Elli-Khan@users.noreply.github.com>
2024-04-02 14:44:37 +0500

    Envs crud added

---
+++ controllers/envs.ts
@@ -0,0 +1,193 @@
+import {
+  BaseController,
+  Controller,
+  Delete,
+  Get,
+  type IRequestContext,
+  type IRoute,
+  Patch,
+  Post,
+  Response,
+  Versioned,
+} from "@Core/common/mod.ts";
+import { type RouterContext, Status } from "oak";
+import e from "validator";
+import { ObjectId } from "mongo";
+
+import { EnvModel, InputEnvSchema } from "@Models/env.ts";
+
+@Controller("/envs/", { name: "envs" })
+export default class EnvsController extends BaseController {
+  @Post("/")
+  public create(route: IRoute) {
+    // Define Body Schema
+    const BodySchema = InputEnvSchema();
+
+    return new Versioned().add("1.0.0", {
+      postman: {
+        body: BodySchema.toSample(),
+      },
+      handler: async (ctx: IRequestContext<RouterContext<string>>) => {
+        // Body Validation
+        const Body = await BodySchema.validate(
+          await ctx.router.request.body({ type: "json" }).value,
+          { name: `${route.scope}.body` },
+        );
+
+        return Response.statusCode(Status.Created).data(
+          await EnvModel.create(Body),
+        );
+      },
+    });
+  }
+
+  @Patch("/:id/")
+  public update(route: IRoute) {
+    // Define Params Schema
+    const ParamsSchema = e.object({
+      id: e.string(),
+    });
+
+    // Define Body Schema
+    const BodySchema = e.partial(InputEnvSchema());
+
+    return new Versioned().add("1.0.0", {
+      postman: {
+        params: ParamsSchema.toSample(),
+        body: BodySchema.toSample(),
+      },
+      handler: async (ctx: IRequestContext<RouterContext<string>>) => {
+        // Params Validation
+        const Params = await ParamsSchema.validate(ctx.router.params, {
+          name: `${route.scope}.params`,
+        });
+
+        // Body Validation
+        const Body = await BodySchema.validate(
+          await ctx.router.request.body({ type: "json" }).value,
+          { name: `${route.scope}.body` },
+        );
+
+        const { modifications } = await EnvModel.updateOneOrFail(
+          Params.id,
+          Body,
+        );
+
+        return Response.data(modifications);
+      },
+    });
+  }
+
+  @Get("/:id?/")
+  public get(route: IRoute) {
+    const CurrentTimestamp = Date.now();
+
+    // Define Query Schema
+    const QuerySchema = e.object(
+      {
+        search: e.optional(e.string()),
+        range: e.optional(
+          e.tuple([e.date().end(CurrentTimestamp), e.date()], { cast: true }),
+        ),
+        offset: e.optional(e.number({ cast: true }).min(0)).default(0),
+        limit: e.optional(e.number({ cast: true }).max(2000)).default(2000),
+        sort: e
+          .optional(
+            e.record(e.number({ cast: true }).min(-1).max(1), { cast: true }),
+          )
+          .default({ _id: -1 }),
+        project: e.optional(
+          e.record(e.number({ cast: true }).min(0).max(1), { cast: true }),
+        ),
+        includeTotalCount: e.optional(
+          e
+            .boolean({ cast: true })
+            .describe(
+              "If `true` is passed, the system will return a total items count for pagination purpose.",
+            ),
+        ),
+      },
+      { allowUnexpectedProps: true },
+    );
+
+    // Define Params Schema
+    const ParamsSchema = e.object({
+      id: e.optional(e.string()),
+    });
+
+    return Versioned.add("1.0.0", {
+      postman: {
+        query: QuerySchema.toSample(),
+        params: ParamsSchema.toSample(),
+      },
+      handler: async (ctx: IRequestContext<RouterContext<string>>) => {
+        // Query Validation
+        const Query = await QuerySchema.validate(
+          Object.fromEntries(ctx.router.request.url.searchParams),
+          { name: `${route.scope}.query` },
+        );
+
+        /**
+         * It is recommended to keep the following validators in place even if you don't want to validate any data.
+         * It will prevent the client from injecting unexpected data into the request.
+         */
+
+        // Params Validation
+        const Params = await ParamsSchema.validate(ctx.router.params, {
+          name: `${route.scope}.params`,
+        });
+
+        const EnvsListQuery = EnvModel
+          .search(Query.search)
+          .filter({
+            ...(Params.id ? { _id: new ObjectId(Params.id) } : {}),
+            ...(Query.range instanceof Array
+              ? {
+                createdAt: {
+                  $gt: new Date(Query.range[0]),
+                  $lt: new Date(Query.range[1]),
+                },
+              }
+              : {}),
+          })
+          .skip(Query.offset)
+          .limit(Query.limit)
+          .sort(Query.sort);
+
+        if (Query.project) EnvsListQuery.project(Query.project);
+
+        return Response.data({
+          totalCount: Query.includeTotalCount
+            //? Make sure to pass any limiting conditions for count if needed.
+            ? await EnvModel.count()
+            : undefined,
+          results: await EnvsListQuery,
+        });
+      },
+    });
+  }
+
+  @Delete("/:id/")
+  public delete(route: IRoute) {
+    // Define Params Schema
+    const ParamsSchema = e.object({
+      id: e.string(),
+    });
+
+    return Versioned.add("1.0.0", {
+      postman: {
+        params: ParamsSchema.toSample(),
+      },
+      handler: async (ctx: IRequestContext<RouterContext<string>>) => {
+        // Params Validation
+        const Params = await ParamsSchema.validate(ctx.router.params, {
+          name: `${route.scope}.params`,
+        });
+
+        await EnvModel.deleteOne(Params.id);
+
+        return Response.true();
+      },
+    });
+  }
+}
--- jobs/envResolver.ts
+++ jobs/envResolver.ts
@@ -3,22 +3,12 @@ import { EnvModel } from "@Models/env.ts";
 
 export default () => {
   // Following function override will resolve undefined env varables from database.
-  Env.onGetFailed = async (key: string) => {
-    const CacheKey = `${Env.getType()}:${key}`;
-    const CachedValue = await Store.get(CacheKey);
-
-    if (typeof CachedValue === "string") return CachedValue;
-
-    const Value = (await EnvModel.findOne({ key }))?.value;
-
-    if (typeof Value === "string") {
-      await Store.set(CacheKey, Value, {
-        expiresInMs: parseFloat(
-          (await Env.get("ENV_CACHE_TTL_MS", true)) ?? "600000", // Default TTL 10 minutes
-        ),
-      });
-    }
-
-    return Value;
-  };
+  Env.onGetFailed = async (key: string) =>
+    await Store.cache(
+      ["env", Env.getType(), key], // Env variable cache key
+      async () => (await EnvModel.findOne({ key }))?.value, // Fetch Env variable
+      parseFloat(
+        (await Env.get("ENV_CACHE_TTL_MS", true)) ?? "600000", // Default TTL 10 minutes
+      ),
+    );
 };
