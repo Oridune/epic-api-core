98bcfddfa42f52c5075cfa8bd04eb790ea92ae60
Saif Ali Khan <saffellikhan@gmail.com>
2024-05-06 11:34:02 +0500

    Minor updates

--- core/middlewares/useTranslator.ts
+++ core/middlewares/useTranslator.ts
@@ -5,7 +5,9 @@ import { Context } from "oak/context.ts";
 import lngParser from "lngParser";
 
 export const useTranslator = async () => {
-  const { translator } = await import("../../i18next.ts");
+  const { I18next } = await import("../../i18next.ts");
+
+  const Translators: Record<string, Context["t"]> = {};
 
   return async (
     ctx: Context<Record<string, any>, Record<string, any>>,
@@ -14,8 +16,21 @@ export const useTranslator = async () => {
     const Languages = lngParser.parse(
       ctx.request.headers.get("accept-language") ?? undefined,
     );
+    const Language = Languages[0]?.code;
 
-    ctx.t = translator(Languages[0]?.code);
+    // Pass the translator to the request context
+    ctx.i18n = I18next;
+
+    ctx.t = Translators[Language] ??= I18next.translator(Language);
+
+    ctx.tvar = (...args: Parameters<typeof ctx.t>) =>
+      Array.from(I18next.availableLanguages).reduce<Record<string, any>>(
+        (trns, lng) => {
+          trns[lng] = (Translators[lng] ??= I18next.translator(lng))(...args);
+          return trns;
+        },
+        {},
+      );
 
     await next();
   };
