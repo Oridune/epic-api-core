d44ae7d27327dce6d9364e2ec8448377157bab94
Saif Ali Khan <saffellikhan@gmail.com>
2024-07-09 18:48:56 +0500

    Wallet management endpoints added

---
+++ controllers/manageWallets.ts
@@ -0,0 +1,270 @@
+import {
+  BaseController,
+  Controller,
+  Delete,
+  Get,
+  type IRequestContext,
+  type IRoute,
+  Post,
+  Response,
+  Versioned,
+} from "@Core/common/mod.ts";
+import { type RouterContext, Status } from "oak";
+import e from "validator";
+import { ObjectId } from "mongo";
+import { Wallet } from "@Lib/wallet.ts";
+
+import { WalletModel } from "@Models/wallet.ts";
+import { TransactionModel } from "@Models/transaction.ts";
+
+@Controller("/manage/wallets/", { name: "manageWallets" })
+export default class ManageWalletsController extends BaseController {
+  @Get("/all/:id?/")
+  public getAll(route: IRoute) {
+    const CurrentTimestamp = Date.now();
+
+    // Define Query Schema
+    const QuerySchema = e.object({
+      search: e.optional(e.string()),
+      range: e.optional(
+        e.tuple([e.date().end(CurrentTimestamp), e.date()], { cast: true }),
+      ),
+      offset: e.optional(e.number({ cast: true }).min(0)).default(0),
+      limit: e.optional(e.number({ cast: true }).max(2000)).default(2000),
+      sort: e.optional(e.record(
+        e.number({ cast: true }).min(-1).max(1),
+        { cast: true },
+      )).default({ _id: -1 }),
+      project: e.optional(
+        e.record(e.number({ cast: true }).min(0).max(1), { cast: true }),
+      ),
+      includeTotalCount: e.optional(
+        e
+          .boolean({ cast: true })
+          .describe(
+            "If `true` is passed, the system will return a total items count for pagination purpose.",
+          ),
+      ),
+    }, { allowUnexpectedProps: true });
+
+    // Define Params Schema
+    const ParamsSchema = e.object({
+      id: e.optional(e.string()),
+    });
+
+    return new Versioned().add("1.0.0", {
+      postman: {
+        query: QuerySchema.toSample(),
+        params: ParamsSchema.toSample(),
+      },
+      handler: async (ctx: IRequestContext<RouterContext<string>>) => {
+        // Query Validation
+        const Query = await QuerySchema.validate(
+          Object.fromEntries(ctx.router.request.url.searchParams),
+          { name: `${route.scope}.query` },
+        );
+
+        /**
+         * It is recommended to keep the following validators in place even if you don't want to validate any data.
+         * It will prevent the client from injecting unexpected data into the request.
+         */
+
+        // Params Validation
+        const Params = await ParamsSchema.validate(ctx.router.params, {
+          name: `${route.scope}.params`,
+        });
+
+        const WalletListQuery = WalletModel
+          .search(Query.search)
+          .filter({
+            ...(Params.id ? { _id: new ObjectId(Params.id) } : {}),
+            ...(Query.range instanceof Array
+              ? {
+                createdAt: {
+                  $gt: new Date(Query.range[0]),
+                  $lt: new Date(Query.range[1]),
+                },
+              }
+              : {}),
+          })
+          .skip(Query.offset)
+          .limit(Query.limit)
+          .sort(Query.sort);
+
+        if (Query.project) WalletListQuery.project(Query.project);
+
+        return Response.data({
+          totalCount: Query.includeTotalCount
+            //? Make sure to pass any limiting conditions for count if needed.
+            ? await WalletModel.count()
+            : undefined,
+          results: await WalletListQuery,
+        });
+      },
+    });
+  }
+
+  @Post("/balance/list/:accountId/")
+  public balanceList(route: IRoute) {
+    // Define Params Schema
+    const ParamsSchema = e.object({
+      accountId: e.string(),
+    });
+
+    // Define Body Schema
+    const BodySchema = e.object({
+      types: e.optional(e.array(e.string())),
+      currencies: e.optional(e.array(e.string())),
+    });
+
+    return Versioned.add("1.0.0", {
+      postman: {
+        params: ParamsSchema.toSample(),
+        body: BodySchema.toSample(),
+      },
+      handler: async (ctx: IRequestContext<RouterContext<string>>) => {
+        // Params Validation
+        const Params = await ParamsSchema.validate(ctx.router.params, {
+          name: `${route.scope}.params`,
+        });
+
+        // Body Validation
+        const Body = await BodySchema.validate(
+          await ctx.router.request.body({ type: "json" }).value,
+          { name: `${route.scope}.body` },
+        );
+
+        return Response.data(
+          await Wallet.list(Params.accountId, Body),
+        );
+      },
+    });
+  }
+
+  @Get("/transactions/:accountId/:type?/:currency?/")
+  public transactions(route: IRoute) {
+    const CurrentTimestamp = Date.now();
+
+    // Define Query Schema
+    const QuerySchema = e.object(
+      {
+        search: e.optional(e.string()),
+        range: e.optional(
+          e.tuple([e.date().end(CurrentTimestamp), e.date()], { cast: true }),
+        ),
+        offset: e.optional(e.number({ cast: true }).min(0)).default(0),
+        limit: e.optional(e.number({ cast: true }).max(2000)).default(2000),
+        sort: e
+          .optional(
+            e.record(e.number({ cast: true }).min(-1).max(1), { cast: true }),
+          )
+          .default({ _id: -1 }),
+        project: e.optional(
+          e.record(e.number({ cast: true }).min(0).max(1), { cast: true }),
+        ),
+        includeTotalCount: e.optional(
+          e
+            .boolean({ cast: true })
+            .describe(
+              "If `true` is passed, the system will return a total items count for pagination purpose.",
+            ),
+        ),
+      },
+      { allowUnexpectedProps: true },
+    );
+
+    // Define Params Schema
+    const ParamsSchema = e.object({
+      accountId: e.string(),
+      type: e.optional(e.string()).default(() => Wallet.getDefaultType()),
+      currency: e
+        .optional(e.string())
+        .default(() => Wallet.getDefaultCurrency()),
+    });
+
+    return Versioned.add("1.0.0", {
+      postman: {
+        query: QuerySchema.toSample(),
+        params: ParamsSchema.toSample(),
+      },
+      handler: async (ctx: IRequestContext<RouterContext<string>>) => {
+        // Query Validation
+        const Query = await QuerySchema.validate(
+          Object.fromEntries(ctx.router.request.url.searchParams),
+          { name: `${route.scope}.query` },
+        );
+
+        // Params Validation
+        const Params = await ParamsSchema.validate(ctx.router.params, {
+          name: `${route.scope}.params`,
+        });
+
+        const TargetAccountId = new ObjectId(Params.accountId);
+
+        const TransactionListBaseConditions = {
+          $or: [
+            { from: TargetAccountId },
+            { to: TargetAccountId },
+          ],
+          type: Params.type,
+          currency: Params.currency,
+          ...(Query.range instanceof Array
+            ? {
+              createdAt: {
+                $gt: new Date(Query.range[0]),
+                $lt: new Date(Query.range[1]),
+              },
+            }
+            : {}),
+        };
+
+        const TransactionListQuery = TransactionModel.search(Query.search)
+          .filter(TransactionListBaseConditions)
+          .skip(Query.offset)
+          .limit(Query.limit)
+          .sort(Query.sort);
+
+        if (Query.project) TransactionListQuery.project(Query.project);
+
+        return Response.data({
+          totalCount: Query.includeTotalCount
+            //? Make sure to pass any limiting conditions for count if needed.
+            ? await TransactionModel.count(TransactionListBaseConditions)
+            : undefined,
+          results: await TransactionListQuery,
+        });
+      },
+    });
+  }
+
+  @Delete("/refund/:id/")
+  public refund(route: IRoute) {
+    // Define Params Schema
+    const ParamsSchema = e.object({
+      id: e.string(),
+    });
+
+    return new Versioned().add("1.0.0", {
+      postman: {
+        params: ParamsSchema.toSample(),
+      },
+      handler: async (ctx: IRequestContext<RouterContext<string>>) => {
+        if (!ctx.router.state.auth) ctx.router.throw(Status.Unauthorized);
+
+        // Params Validation
+        const Params = await ParamsSchema.validate(ctx.router.params, {
+          name: `${route.scope}.params`,
+        });
+
+        const Transfer = await Wallet.refund({
+          transactionId: Params.id,
+          user: ctx.router.state.auth.userId,
+        });
+
+        return Response.data({
+          transaction: Transfer.transaction,
+        });
+      },
+    });
+  }
+}
