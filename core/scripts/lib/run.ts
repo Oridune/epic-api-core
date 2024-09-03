8db63b3e6c1d52bbd478082949ccb779f873b7b7
Saif Ali Khan <saffellikhan@gmail.com>
2024-09-03 15:27:22 +0500

    Core updated & SimpleWebAuthn source changed to npm

--- core/scripts/lib/run.ts
+++ core/scripts/lib/run.ts
@@ -1,3 +1,13 @@
+import {
+  exec as _exec,
+  spawn as _spawn,
+  SpawnOptionsWithoutStdio,
+} from "node:child_process";
+import { promisify } from "node:util";
+import { nodeReadableToDenoReadableStream, printStream } from "./utility.ts";
+
+export const exec = promisify(_exec);
+
 export const run = async (
   command: string | URL,
   options?: Deno.CommandOptions,
@@ -7,4 +17,17 @@ export const run = async (
   const Process = Command.spawn();
 
   return await Process.status;
+};
+
+export const spawn = async (
+  command: string,
+  options?: SpawnOptionsWithoutStdio,
+) => {
+  const [executable, ...args] = command.split(" ");
+  const Process = _spawn(executable, args, options);
+
+  return await Promise.all([
+    printStream(nodeReadableToDenoReadableStream(Process.stdout)),
+    printStream(nodeReadableToDenoReadableStream(Process.stderr)),
+  ]);
 };
