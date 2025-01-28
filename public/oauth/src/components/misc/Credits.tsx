88ed0706632725dfe52f4fa86f9fae45d5939bdc
Saif Ali Khan <62799255+Saff-Elli-Khan@users.noreply.github.com>
2025-01-28 18:25:45 +0500

    UI updated

--- public/oauth/src/components/misc/Credits.tsx
+++ public/oauth/src/components/misc/Credits.tsx
@@ -5,17 +5,32 @@ import Logo from "../../assets/logo.png";
 export const Credits: React.FC<{ message?: string }> = ({ message }) => {
   const { t } = useTranslation();
 
+  // return (
+  //   <Typography variant="body2" align="center" sx={{ mb: "20px" }}>
+  //     <Link color="#e85d04" href="https://oridune.com" target="_blank">
+  //       <img
+  //         style={{ verticalAlign: "middle" }}
+  //         width={20}
+  //         height={20}
+  //         src={Logo}
+  //         alt="Oridune"
+  //       />{" "}
+  //       {message ?? t("Powered by Oridune")}
+  //     </Link>
+  //   </Typography>
+  // );
+
   return (
     <Typography variant="body2" align="center" sx={{ mb: "20px" }}>
-      <Link color="#e85d04" href="https://oridune.com" target="_blank">
+      <Link color="#e85d04" href="https://huruftech.com" target="_blank">
         <img
           style={{ verticalAlign: "middle" }}
           width={20}
           height={20}
           src={Logo}
-          alt="Oridune"
+          alt="Huruf Tech"
         />{" "}
-        {message ?? t("Powered by Oridune")}
+        {message ?? t("Powered by Huruf Tech")}
       </Link>
     </Typography>
   );
