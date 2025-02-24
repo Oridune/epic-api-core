4e3d65b3a6688ec63c8b37bc32dc57d036b846ef
Saif Ali Khan <62799255+Saff-Elli-Khan@users.noreply.github.com>
2025-02-24 18:42:25 +0500

    Added a new endpoint manageUsers.updatePassword

---
+++ controllers/manageUsers.ts
@@ -0,0 +1,66 @@
+import {
+  BaseController,
+  Controller,
+  type IRequestContext,
+  type IRoute,
+  Put,
+  Response,
+  Versioned,
+} from "@Core/common/mod.ts";
+import { type RouterContext } from "oak";
+import e from "validator";
+import { ObjectId } from "mongo";
+import { PasswordValidator, UserModel } from "@Models/user.ts";
+import { hash as bcryptHash } from "bcrypt";
+
+@Controller("/manage/users/", { name: "manageUsers" })
+export default class ManageUsersController extends BaseController {
+  @Put("/password/:id/")
+  public updatePassword(route: IRoute) {
+    // Define Params Schema
+    const ParamsSchema = e.object({
+      id: e.instanceOf(ObjectId, { instantiate: true }),
+    });
+
+    // Define Body Schema
+    const BodySchema = e.object({
+      password: PasswordValidator(),
+      hashedPassword: e
+        .any()
+        .custom((ctx) => bcryptHash(ctx.parent?.output.password)),
+    });
+
+    return new Versioned().add("1.0.0", {
+      shape: () => ({
+        params: ParamsSchema.toSample(),
+        body: BodySchema.toSample(),
+      }),
+      handler: async (ctx: IRequestContext<RouterContext<string>>) => {
+        // Params Validation
+        const Params = await ParamsSchema.validate(ctx.router.params, {
+          name: `${route.scope}.params`,
+        });
+
+        // Body Validation
+        const Body = await BodySchema.validate(
+          await ctx.router.request.body.json(),
+          { name: `${route.scope}.body` },
+        );
+
+        await UserModel.updateOneOrFail(
+          Params.id,
+          {
+            password: Body.hashedPassword,
+            $push: {
+              passwordHistory: Body.hashedPassword,
+            },
+            failedLoginAttempts: 0,
+            deletionAt: null,
+          },
+        );
+
+        return Response.true();
+      },
+    });
+  }
+}
