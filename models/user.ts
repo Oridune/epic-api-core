4b7a39dcd1e2b15560d8b756218f2dbcdc430860
Saif Ali Khan <62799255+Saff-Elli-Khan@users.noreply.github.com>
2025-06-18 15:37:08 +0500

    Changed phone validator regex

--- models/user.ts
+++ models/user.ts
@@ -41,8 +41,7 @@ export const EmailValidator = () =>
 
 export const PhoneValidator = () =>
   e.string().matches({
-    regex:
-      /^\+((?:9[679]|8[035789]|6[789]|5[90]|42|3[578]|2[1-689])|9[0-58]|8[1246]|6[0-6]|5[1-8]|4[013-9]|3[0-469]|2[70]|7|1)(?:\W*\d){7,13}\d$/,
+    regex: /^\+[1-9]\d{6,14}$/,
   });
 
 export enum Gender {
