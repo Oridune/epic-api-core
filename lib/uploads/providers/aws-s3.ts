110616d44bec9817c525cdadd001e89e350b4a0e
Saif Ali Khan <62799255+Saff-Elli-Khan@users.noreply.github.com>
2025-07-12 16:42:02 +0500

    Minor fixes

--- lib/uploads/providers/aws-s3.ts
+++ lib/uploads/providers/aws-s3.ts
@@ -1,11 +1,11 @@
 import { Env } from "@Core/common/env.ts";
 import { join } from "path";
 import {
-  S3Client,
-  PutObjectCommand,
+  DeleteObjectCommand,
   GetObjectCommand,
   HeadObjectCommand,
-  DeleteObjectCommand,
+  PutObjectCommand,
+  S3Client,
 } from "aws-s3";
 import { getSignedUrl } from "aws-s3-presigner";
 import {
@@ -22,9 +22,15 @@ export class AwsS3Provider {
 
   static async getClient(region?: string) {
     const [Region, AccessId, AccessKey] = await Promise.all([
-      Env.get("AWS_S3_REGION"),
-      Env.get("AWS_S3_ACCESS_ID"),
-      Env.get("AWS_S3_ACCESS_KEY"),
+      (async () =>
+        (await Env.get("AWS_S3_REGION", true)) ??
+          await Env.get("AWS_REGION"))(),
+      (async () =>
+        (await Env.get("AWS_S3_ACCESS_ID", true)) ??
+          await Env.get("AWS_ACCESS_ID"))(),
+      (async () =>
+        (await Env.get("AWS_S3_ACCESS_KEY", true)) ??
+          await Env.get("AWS_ACCESS_KEY"))(),
     ]);
 
     return (this.Client ??= new S3Client({
@@ -37,11 +43,11 @@ export class AwsS3Provider {
   }
 
   static async signUploadUrl(
-    options?: Omit<ISignUploadUrlOptions, "provider">
+    options?: Omit<ISignUploadUrlOptions, "provider">,
   ) {
     // Gather information
-    const BucketName =
-      options?.bucketName ?? (await Env.get("AWS_S3_BUCKET_NAME"));
+    const BucketName = options?.bucketName ??
+      (await Env.get("AWS_S3_BUCKET_NAME"));
     const Location = options?.location ?? "";
     const Key = join(Location, Utils.uuidV4()).replace(/\\/g, "/");
     const ACL = options?.awsS3ACL ?? AwsS3ACLs.PUBLIC_READ;
@@ -61,7 +67,7 @@ export class AwsS3Provider {
       {
         expiresIn: ExpiresInSeconds,
         signableHeaders: new Set(["content-type", "content-length"]),
-      }
+      },
     );
 
     const ObjectUrl = await this.parseObjectUrl(UploadUrl);
@@ -76,15 +82,14 @@ export class AwsS3Provider {
 
   static async signGetUrl(
     objectUrl: string | Omit<IParsedObjectUrl, "provider">,
-    options?: Omit<IObjectLocation, "provider"> & { expiresInMs?: number }
+    options?: Omit<IObjectLocation, "provider"> & { expiresInMs?: number },
   ) {
     // Gather information
-    const BucketName =
-      options?.bucketName ?? (await Env.get("AWS_S3_BUCKET_NAME"));
-    const ObjectUrl =
-      typeof objectUrl === "object"
-        ? objectUrl
-        : await this.parseObjectUrl(objectUrl, options);
+    const BucketName = options?.bucketName ??
+      (await Env.get("AWS_S3_BUCKET_NAME"));
+    const ObjectUrl = typeof objectUrl === "object"
+      ? objectUrl
+      : await this.parseObjectUrl(objectUrl, options);
     const ExpiresInSeconds = (options?.expiresInMs ?? 3600000) / 1000; // Default 1 hour
 
     return {
@@ -95,7 +100,7 @@ export class AwsS3Provider {
           Bucket: BucketName,
           Key: ObjectUrl.key,
         }),
-        { expiresIn: ExpiresInSeconds }
+        { expiresIn: ExpiresInSeconds },
       ),
       expiresInSeconds: ExpiresInSeconds,
     };
@@ -103,22 +108,21 @@ export class AwsS3Provider {
 
   static async getObject(
     objectUrl: string | Omit<IParsedObjectUrl, "provider">,
-    options?: Omit<IObjectLocation, "provider">
+    options?: Omit<IObjectLocation, "provider">,
   ) {
     // Gather information
-    const BucketName =
-      options?.bucketName ?? (await Env.get("AWS_S3_BUCKET_NAME"));
-    const ObjectUrl =
-      typeof objectUrl === "object"
-        ? objectUrl
-        : await this.parseObjectUrl(objectUrl, options);
+    const BucketName = options?.bucketName ??
+      (await Env.get("AWS_S3_BUCKET_NAME"));
+    const ObjectUrl = typeof objectUrl === "object"
+      ? objectUrl
+      : await this.parseObjectUrl(objectUrl, options);
     const Client = await this.getClient(options?.region);
 
     const Results = await Client.send(
       new GetObjectCommand({
         Bucket: BucketName,
         Key: ObjectUrl.key,
-      })
+      }),
     );
 
     const Stream = await Results.Body?.transformToWebStream();
@@ -130,22 +134,21 @@ export class AwsS3Provider {
 
   static async objectExists(
     objectUrl: string | Omit<IParsedObjectUrl, "provider">,
-    options?: Omit<IObjectLocation, "provider">
+    options?: Omit<IObjectLocation, "provider">,
   ) {
     // Gather information
-    const BucketName =
-      options?.bucketName ?? (await Env.get("AWS_S3_BUCKET_NAME"));
-    const ObjectUrl =
-      typeof objectUrl === "object"
-        ? objectUrl
-        : await this.parseObjectUrl(objectUrl, options);
+    const BucketName = options?.bucketName ??
+      (await Env.get("AWS_S3_BUCKET_NAME"));
+    const ObjectUrl = typeof objectUrl === "object"
+      ? objectUrl
+      : await this.parseObjectUrl(objectUrl, options);
     const Client = await this.getClient(options?.region);
 
     return Client.send(
       new HeadObjectCommand({
         Bucket: BucketName,
         Key: ObjectUrl.key,
-      })
+      }),
     )
       .then((result) => result.$metadata.httpStatusCode === 200)
       .catch(() => false);
@@ -153,32 +156,31 @@ export class AwsS3Provider {
 
   static async deleteObject(
     objectUrl: string | Omit<IParsedObjectUrl, "provider">,
-    options?: Omit<IObjectLocation, "provider">
+    options?: Omit<IObjectLocation, "provider">,
   ) {
-    const BucketName =
-      options?.bucketName ?? (await Env.get("AWS_S3_BUCKET_NAME"));
-    const ObjectUrl =
-      typeof objectUrl === "object"
-        ? objectUrl
-        : await this.parseObjectUrl(objectUrl, options);
+    const BucketName = options?.bucketName ??
+      (await Env.get("AWS_S3_BUCKET_NAME"));
+    const ObjectUrl = typeof objectUrl === "object"
+      ? objectUrl
+      : await this.parseObjectUrl(objectUrl, options);
     const Client = await this.getClient(options?.region);
 
     return Client.send(
       new DeleteObjectCommand({
         Bucket: BucketName,
         Key: ObjectUrl.key,
-      })
+      }),
     );
   }
 
   static async parseObjectUrl(
     objectUrl: string,
-    options?: Omit<IObjectLocation, "provider">
+    options?: Omit<IObjectLocation, "provider">,
   ): Promise<IParsedObjectUrl> {
     const ResolveUrl = objectUrl.split("?")[0];
     const Region = options?.region ?? (await Env.get("AWS_S3_REGION"));
-    const BucketName =
-      options?.bucketName ?? (await Env.get("AWS_S3_BUCKET_NAME"));
+    const BucketName = options?.bucketName ??
+      (await Env.get("AWS_S3_BUCKET_NAME"));
     const S3Url = new URL(ResolveUrl);
     const S3Host = `${BucketName}.s3.${Region}.amazonaws.com`;
 
@@ -190,10 +192,11 @@ export class AwsS3Provider {
         .join("/");
       const CheckLocation = options?.location?.replace(/^\/|\/$/g, "");
 
-      if (CheckLocation && UploadedLocation !== CheckLocation)
+      if (CheckLocation && UploadedLocation !== CheckLocation) {
         throw new Error(
-          `Invalid upload location '${UploadedLocation}'! Expected location is '${CheckLocation}'`
+          `Invalid upload location '${UploadedLocation}'! Expected location is '${CheckLocation}'`,
         );
+      }
 
       return {
         provider: UploadsProvider.AWS_S3,
@@ -204,7 +207,7 @@ export class AwsS3Provider {
     }
 
     throw new Error(
-      `Invalid upload URL host '${S3Url.host}' has been provided! Expected host is '${S3Host}'!`
+      `Invalid upload URL host '${S3Url.host}' has been provided! Expected host is '${S3Host}'!`,
     );
   }
 }
