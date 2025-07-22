53f8d28cedf2043cf12df798cc297a7db06cf1ec
Saif Ali Khan <62799255+Saff-Elli-Khan@users.noreply.github.com>
2025-07-22 20:02:38 +0500

    Minor fixes

--- public/oauth/src/components/misc/OTPVerifier.tsx
+++ public/oauth/src/components/misc/OTPVerifier.tsx
@@ -20,120 +20,119 @@ export interface IOTPModelProps {
   onVerify: (challengeToken: string, code: string) => Promise<string>;
 }
 
-export const OTPVerifier = React.forwardRef<
-  IOTPModelRef,
-  IOTPModelProps
->(({ onVerify }, ref) => {
-  const { t } = useTranslation();
+export const OTPVerifier = React.forwardRef<IOTPModelRef, IOTPModelProps>(
+  ({ onVerify }, ref) => {
+    const { t } = useTranslation();
 
-  const [challengeToken, setChallengeToken] = React.useState<string | null>(
-    null
-  );
+    const [challengeToken, setChallengeToken] = React.useState<string | null>(
+      null
+    );
 
-  const [otp, setOTP] = React.useState("");
+    const [otp, setOTP] = React.useState("");
 
-  const [loading, setLoading] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
 
-  React.useImperativeHandle(ref, () => ({
-    verify(token) {
-      setChallengeToken(token);
+    React.useImperativeHandle(ref, () => ({
+      verify(token) {
+        setChallengeToken(token);
 
-      return new Promise((resolve, reject) => {
-        addEventListener(
-          "__otpVerification",
-          (event) => {
-            const ev = event as CustomEvent<{
-              success: boolean;
-              message: string;
-              token: string;
-            }>;
+        return new Promise((resolve, reject) => {
+          addEventListener(
+            "__otpVerification",
+            (event) => {
+              const ev = event as CustomEvent<{
+                success: boolean;
+                message: string;
+                token: string;
+              }>;
 
-            if (ev.detail.success) {
-              resolve(ev.detail.token);
+              if (ev.detail.success) {
+                resolve(ev.detail.token);
 
-              return;
-            }
+                return;
+              }
 
-            reject(new Error(ev.detail.message ?? "Something went wrong!"));
-          },
-          { once: true }
+              reject(new Error(ev.detail.message ?? "Something went wrong!"));
+            },
+            { once: true }
+          );
+        });
+      },
+    }));
+
+    const handleClose = () => {
+      setChallengeToken(null);
+    };
+
+    const handleVerify = async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      setLoading(true);
+
+      try {
+        if (!challengeToken) throw new Error("No challenge token!");
+
+        const token = await onVerify(challengeToken, otp);
+
+        dispatchEvent(
+          new CustomEvent("__otpVerification", {
+            detail: {
+              success: true,
+              token,
+            },
+          })
         );
-      });
-    },
-  }));
+      } catch (err) {
+        dispatchEvent(
+          new CustomEvent("__otpVerification", {
+            detail: {
+              success: false,
+              message: err instanceof Error ? err.message : String(err),
+            },
+          })
+        );
+      } finally {
+        handleClose();
+      }
 
-  const handleClose = () => {
-    setChallengeToken(null);
-  };
+      setLoading(false);
+    };
 
-  const handleVerify = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    setLoading(true);
-
-    try {
-      if (!challengeToken) throw new Error("No challenge token!");
-
-      const token = await onVerify(challengeToken, otp);
-
-      dispatchEvent(
-        new CustomEvent("__otpVerification", {
-          detail: {
-            success: true,
-            token,
-          },
-        })
-      );
-    } catch (err) {
-      dispatchEvent(
-        new CustomEvent("__otpVerification", {
-          detail: {
-            success: false,
-            message: err instanceof Error ? err.message : String(err),
-          },
-        })
-      );
-    } finally {
-      handleClose();
-    }
-
-    setLoading(false);
-  };
-
-  return (
-    <Dialog open={!!challengeToken} onClose={handleClose}>
-      <DialogTitle>{t("Verify OTP")}</DialogTitle>
-      <DialogContent sx={{ paddingBottom: 0 }}>
-        <DialogContentText>
-          {t("Enter the OTP code from your configured authenticator device.")}
-        </DialogContentText>
-        <form onSubmit={handleVerify}>
-          <FormControl fullWidth variant="outlined" sx={{ marginY: 2 }}>
-            <InputLabel htmlFor="code">{t("OTP Code")}</InputLabel>
-            <OutlinedInput
-              id="code"
-              label={t("OTP Code")}
-              type="number"
-              autoComplete="code"
-              inputProps={{
-                min: "100000",
-                max: "999999",
-              }}
-              value={otp}
-              onChange={(e) => {
-                setOTP(e.target.value);
-              }}
-              required
-            />
-          </FormControl>
-          <DialogActions>
-            <Button onClick={handleClose}>{t("Cancel")}</Button>
-            <Button type="submit" variant="contained" disabled={loading}>
-              {t("Verify")}
-            </Button>
-          </DialogActions>
-        </form>
-      </DialogContent>
-    </Dialog>
-  );
-});
+    return (
+      <Dialog open={!!challengeToken} onClose={handleClose}>
+        <DialogTitle>{t("Verify OTP")}</DialogTitle>
+        <DialogContent sx={{ paddingBottom: 0 }}>
+          <DialogContentText>
+            {t("Enter the OTP code from your configured authenticator device.")}
+          </DialogContentText>
+          <form onSubmit={handleVerify}>
+            <FormControl fullWidth variant="outlined" sx={{ marginY: 2 }}>
+              <InputLabel htmlFor="code">{t("OTP Code")}</InputLabel>
+              <OutlinedInput
+                id="code"
+                label={t("OTP Code")}
+                type="text"
+                autoComplete="code"
+                inputProps={{
+                  minLength: 6,
+                  maxLength: 6,
+                }}
+                value={otp}
+                onChange={(e) => {
+                  setOTP(e.target.value);
+                }}
+                required
+              />
+            </FormControl>
+            <DialogActions>
+              <Button onClick={handleClose}>{t("Cancel")}</Button>
+              <Button type="submit" variant="contained" disabled={loading}>
+                {t("Verify")}
+              </Button>
+            </DialogActions>
+          </form>
+        </DialogContent>
+      </Dialog>
+    );
+  }
+);
